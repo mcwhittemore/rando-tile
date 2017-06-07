668d81aaa0259c4013a01ee151e0ad139435c9aa
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-06-06 22:08:52 -0400

    ...

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ module.exports = function() {
   var lng = 180-(Math.random()*360);
   var lat = 90-(Math.random()*180);
   var zoom = Math.floor(Math.random()*20);
-  console.log(lng, lat, zoom);
   return tilebelt.pointToTile(lng, lat, zoom);
 }
